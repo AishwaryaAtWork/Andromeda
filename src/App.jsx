fe1bfb1a6d679cccc0454de8c58de28ebd28f7c1
1byx <prj2cmh1@duck.com>
2023-03-18 18:51:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css';
+import React from 'react';
+import CardComponent from './components/CardComponent';
+import NavBar from './components/NavBar';
+import internshipsData from '../internshipsData';
+import Carousels from './components/Carousels';
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center bg-blue-500">Hello world!</h1>
+      <div> Hello</div>
     </>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-primary text-white">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" internshipsData-bs-toggle="collapse" internshipsData-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Contact us</a>
+                        </li>
+                    </ul>
+                    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                nsl
+                                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-dark">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
