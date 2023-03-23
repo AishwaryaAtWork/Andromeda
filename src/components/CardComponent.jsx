dfd030809ab2030d859520c988ee7aab9173d5b7
Ayush <ayushaggarwal42003@gmail.com>
2023-03-23 22:05:38 +0530

    talwind css cdn

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import CardItem from './Card';
+import React from "react";
+import CardItem from "./Card";
 
 export default function CardComponent(props) {
-    const { internships } = props.internshipsData;
-    return (
-        <div className='container'>
-            <div className='row'>
-                {internships.map((internship, index) => (
-                    <div className='col-md-4 my-3' keys={internships.id}>
-                        <CardItem internship={internship} key={index} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  const { internships } = props.internshipsData;
+  return (
+    <div className="container">
+      <div className="row">
+        {internships.map((internship, index) => (
+          <div className="col-md-4 my-3" keys={internships.id}>
+            <CardItem internship={internship} key={index} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
