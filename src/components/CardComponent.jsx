6ff0c294aa763b2eaee0844e4c4703c0e3bf4d57
1byx <prj2cmh1@duck.com>
2023-03-24 20:45:03 +0530

    Update

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -4,14 +4,7 @@ import CardItem from "./Card";
 export default function CardComponent(props) {
   const { internships } = props.internshipsData;
   return (
-    <div className="container">
-      <div className="row">
-        {internships.map((internship, index) => (
-          <div className="col-md-4 my-3" keys={internships.id}>
-            <CardItem internship={internship} key={index} />
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+    </>
   );
 }
