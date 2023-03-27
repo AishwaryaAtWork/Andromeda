ae4a4f74ea43dafb0c0255f57df355448ba67823
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-03-27 18:07:15 +0530

    Loading spinner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import React from "react";
-// import CardComponent from "./components/CardComponent";
+import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
-// import internshipsData from "../internshipsData";
-// import apprenticeshipsData from "../apprenticeshipsData";
 import sliderImagesData from "./constants/sliderImagesData";
 import opportunityTypeData from "./constants/opportunityTypeData";
 import Carousels from "./components/Carousels";
 import LowerNavbar from "./components/LowerNavbar";
-// import Card from "./components/Card";
+import BounceLoader from "react-spinners/BounceLoader";
+import './App.css';
 
 function App() {
+  const [loading,setLoading] = useState(false);
+  useEffect(()=>{
+    setLoading(true);
+    setTimeout(()=>{
+      setLoading(false);
+    },3000);
+  },[]);
   return (
     <>
-      <NavBar />
-      <Carousels sliderImagesData={sliderImagesData}/>
-      <LowerNavbar opportunityTypeData={opportunityTypeData}/>
-      {/* <CardComponent apprenticeshipsData={apprenticeshipsData} /> */}
-      {/* <Card internshipsData={internshipsData}/> */}
+      {
+        loading ? 
+          <div className="loader">
+            <BounceLoader color={"#3b82f6"} loading={loading} size={90}/>
+          </div>
+        :
+        <>
+          <NavBar />
+          <Carousels sliderImagesData={sliderImagesData}/>
+          <LowerNavbar opportunityTypeData={opportunityTypeData}/>
+        </>
+      }
     </>
   );
 }
