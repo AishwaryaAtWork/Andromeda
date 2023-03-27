f8d3b4e6715162f7991f22ebace336a05f12a1e2
1byx <prj2cmh1@duck.com>
2023-03-28 00:01:01 +0530

    Refactor

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -4,25 +4,13 @@ import Card from "./Card";
 export default function CardComponent(props) {
   const { apprenticeships } = props.apprenticeshipsData;
   const { internships } = props.internshipsData;
+  const opportunityType = props.type.toLowerCase() === 'apprenticeships' ? apprenticeships : internships;
 
-  // To decide opportunity on lower navbar click
-  if (props.type.toLowerCase() === 'internships') {
-    var OpportunityType = internships;
-  }
-  else if (props.type.toLowerCase() === 'apprenticeships') {
-    var OpportunityType = apprenticeships;
-  }
-  else {
-    var OpportunityType = internships;
-  }
   return (
-    <>
-      {/* Card container div  */}
-      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 justify-items-center'>
-        {OpportunityType.map((i) => (
-          <Card i={i} key={i.id} />
-        ))}
-      </div>
-    </>
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 justify-items-center'>
+      {opportunityType.map(opportunity => (
+        <Card i={opportunity} key={opportunity.id} />
+      ))}
+    </div>
   );
 }
