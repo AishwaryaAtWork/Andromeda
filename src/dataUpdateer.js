6dfbeacb25c2e07674a92fdbcf8fea0db517210c
1byx <prj2cmh1@duck.com>
2023-04-01 11:28:42 +0000

    Apr 1, 2023, 4:58 PM

--- src/dataUpdateer.js
+++ src/dataUpdateer.js
@@ -1,20 +1,16 @@
-// Check for outdated data
-function checkForOutdatedData(data) {
-    const currentDate = new Date();
-    return data.filter(item => {
-        const applyByDate = new Date(item.apply_by);
-        return applyByDate < currentDate;
-    });
-}
+const fs = require('fs');
+const currentDate = new Date();
 
-// Remove outdated data and rewrite JSON file
-function removeOutdatedData() {
-    const jsonData = require('./internshipsData.json');
-    const outdatedData = checkForOutdatedData(jsonData.internships);
-    const updatedData = jsonData.internships.filter(item => !outdatedData.includes(item));
-    const updatedJsonData = { internships: updatedData };
-    fs.writeFileSync('./data.json', JSON.stringify(updatedJsonData));
-}
+// Read data from the JSON file
+const rawData = fs.readFileSync('internships.json');
+const data = JSON.parse(rawData);
 
-// Schedule function to run periodically
-setInterval(removeOutdatedData, 86400000); // Runs once a day
+// Filter out items with expired apply_by dates
+const filteredData = data.internships.filter(item => {
+    const applyByDate = new Date(item.apply_by);
+    return applyByDate >= currentDate;
+});
+
+// Write the updated data back to the JSON file
+data.internships = filteredData;
+fs.writeFileSync('internships.json', JSON.stringify(data));
