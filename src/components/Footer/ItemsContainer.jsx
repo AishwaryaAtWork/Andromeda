55b848438576cf0e999c4049e55dee2d977a3a14
1byx <prj2cmh1@duck.com>
2023-04-01 09:02:26 +0000

    Apr 1, 2023, 2:32 PM

--- src/components/Footer/ItemsContainer.jsx
+++ src/components/Footer/ItemsContainer.jsx
@@ -1,5 +1,6 @@
 import Item from "./Item";
 import { TEAM, RESOURCES, COMPANIES, SUPPORT } from "./Menus";
+
 const ItemsContainer = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
